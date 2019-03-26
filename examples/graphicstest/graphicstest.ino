1bfe358c9d2a1ffb8101c7bfbb29736db88d4835
LilyGO <31160602+LilyGO@users.noreply.github.com>
2019-03-26 17:01:31 +0800

    Update graphicstest.ino

--- examples/graphicstest/graphicstest.ino
+++ examples/graphicstest/graphicstest.ino
@@ -19,13 +19,14 @@
 #include "Adafruit_ILI9341.h"
 
 // For the Adafruit shield, these are the default.
-#define TFT_DC 9
-#define TFT_CS 10
 
-// Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
-// If using the breakout, change pins as desired
-//Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
+#define TFT_CS   27
+#define TFT_DC   26
+#define TFT_MOSI 23
+#define TFT_CLK  18
+#define TFT_RST  5
+#define TFT_MISO 12
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
 
 void setup() {
   Serial.begin(9600);
